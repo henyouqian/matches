98bb3877a9af40bb590b193e43c3b6c3becfd6b5
liwei <henyouqian@gmail.com>
2013-09-11 18:30:21 +0800

    lw

--- util.go
+++ util.go
@@ -4,14 +4,20 @@ import (
 	"crypto/sha256"
 	"database/sql"
 	"encoding/base64"
+	"encoding/csv"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/garyburd/redigo/redis"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/golang/glog"
 	"github.com/nu7hatch/gouuid"
 	"net/http"
+	"os"
+	"reflect"
 	"runtime"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -165,6 +171,109 @@ func repeatSingletonTask(key string, interval time.Duration, f func()) {
 
 		time.Sleep(1 * time.Second)
 	}
+}
+
+func loadCsvTbl(file string, keycols []string, tbl interface{}) (err error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	//
+	v := reflect.ValueOf(tbl).Elem()
+	defer func() {
+		if r := recover(); r != nil {
+			err = errors.New(fmt.Sprintf("tbl's type must be map[string]struct. detail:%v", r))
+		}
+	}()
+
+	t := v.Type()
+	if v.IsNil() {
+		v.Set(reflect.MakeMap(t))
+	}
+	rowObjType := t.Elem()
+
+	//
+	reader := csv.NewReader(f)
+	firstrow, err := reader.Read()
+	keycolidxs := make([]int, len(keycols))
+	for icol, vcol := range keycols {
+		found := false
+		for i, v := range firstrow {
+			if strings.EqualFold(v, vcol) {
+				keycolidxs[icol] = i
+				found = true
+				break
+			}
+		}
+		if !found {
+			return errors.New(fmt.Sprintf("column not found: %s in %s", vcol, file))
+		}
+	}
+
+	if len(keycolidxs) != len(keycols) {
+		errors.New(fmt.Sprintf("keys not match totally: keycols = %v", keycols))
+	}
+
+	row, err := reader.Read()
+	for row != nil {
+		rowobjValue := reflect.New(rowObjType).Elem()
+		numField := rowobjValue.NumField()
+		for i := 0; i < numField; i++ {
+			f := rowobjValue.Field(i)
+			colname := rowobjValue.Type().Field(i).Name
+
+			colidx := -1
+			for i, v := range firstrow {
+				if strings.EqualFold(colname, v) {
+					colidx = i
+					break
+				}
+			}
+			if colidx != -1 {
+				valstr := row[colidx]
+				switch f.Kind() {
+				case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+					n, err := strconv.ParseInt(valstr, 0, 64)
+					if err != nil {
+						return err
+					}
+					f.SetInt(n)
+				case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+					n, err := strconv.ParseUint(valstr, 0, 64)
+					if err != nil {
+						return err
+					}
+					f.SetUint(n)
+				case reflect.Float32, reflect.Float64:
+					n, err := strconv.ParseFloat(valstr, 64)
+					if err != nil {
+						return err
+					}
+					f.SetFloat(n)
+				case reflect.Bool:
+					n, err := strconv.ParseBool(valstr)
+					if err != nil {
+						return err
+					}
+					f.SetBool(n)
+				case reflect.String:
+					f.SetString(valstr)
+				}
+			}
+		}
+
+		keys := make([]string, len(keycolidxs))
+		for i, v := range keycolidxs {
+			keys[i] = row[v]
+		}
+		v.SetMapIndex(reflect.ValueOf(strings.Join(keys, ",")), rowobjValue)
+
+		row, err = reader.Read()
+	}
+
+	return nil
 }
 
 // just for test
